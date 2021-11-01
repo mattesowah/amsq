7d957c491dc8c13a80afd6d29aa418cabe53af2c
a17labs <*a17matte@gmail.com*>
2021-11-01 02:49:40 +0000

    changes

--- slider/app.js
+++ slider/app.js
@@ -20,20 +20,6 @@ for (let i = 0; i < 25; i++) {
 
 const brt = new PIXI.BaseRenderTexture(300, 300, PIXI.SCALE_MODES.LINEAR, 1);
 const rt = new PIXI.RenderTexture(brt);
-
-const sprite = new PIXI.Sprite(rt);
-
-sprite.x = 450;
-sprite.y = 60;
-app.stage.addChild(sprite);
-
-/*
- * All the bunnies are added to the container with the addChild method
- * when you do this, all the bunnies become children of the container, and when a container moves,
- * so do all its children.
- * This gives you a lot of flexibility and makes it easier to position elements on the screen
- */
-container.x = 100;
 container.y = 60;
 
 app.ticker.add(() => {
